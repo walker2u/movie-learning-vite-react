5b6b9d4eca290531325f0cce8677a3aef7710227
walker2u <kumar99prasad@gmail.com>
2025-05-18 23:58:50 +0530

    added errorhandler middleware

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./modules/user/user.route.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use("/api/user", userRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");

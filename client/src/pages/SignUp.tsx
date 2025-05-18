9fed8434ab777ef04f333d24b09d6ed35dfd13da
walker2u <kumar99prasad@gmail.com>
2025-05-19 00:10:52 +0530

    added api call in fronted

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,9 +1,32 @@
 import { useNavigate } from "react-router-dom";
 
+import { useState } from "react";
+
 function SignUp() {
   const navigate = useNavigate();
-  const handleRegister = () => {
-    navigate("/");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleRegister = async () => {
+    try {
+      console.log(email, password);
+      const res = await fetch("http://localhost:3000/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -12,10 +35,20 @@ function SignUp() {
         <legend className="fieldset-legend text-2xl">Register</legend>
 
         <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
+        <input
+          type="email"
+          className="input"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
+        <input
+          type="password"
+          className="input"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button className="btn btn-neutral mt-4" onClick={handleRegister}>
           Register
